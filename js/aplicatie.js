4c62b600bbe50de61a947d35727dc6454a080b66
TudorDan <tudor.ist@gmail.com>
2019-04-09 22:23:12 +0300

    Add cart page content

--- js/aplicatie.js
+++ js/aplicatie.js
@@ -1,6 +1,6 @@
 function preiaProdusele() {
     let url = "https://magazinelectronic-fa84a.firebaseio.com/produse.json";
-    fetch(url)
+    return fetch(url)
         .then(response => {
             if(response.ok) {
                 return response;
@@ -11,14 +11,14 @@ function preiaProdusele() {
         .then(produse => {
             //console.log(produse);
             let zonaProduse = document.querySelector('#produse');
-            for(id in produse) {
+            for(let i in produse) {
                 zonaProduse.innerHTML += `
                     <div class="articolComercial">
-                        <img src="${produse[id].imagine}">
-                        <h2>${produse[id].nume}</h2>
-                        <p>${produse[id].pret}&nbsp;&euro;</p>
+                        <img src="${produse[i].imagine}">
+                        <h2>${produse[i].nume}</h2>
+                        <p>${produse[i].pret}&nbsp;&euro;</p>
                         <button>
-                            <a href="details.html?id=${id}">Detalii</a>
+                            <a href="details.html?id=${i}">Detalii</a>
                         </button>
                     </div>
                 `;
@@ -32,7 +32,7 @@ function preiaProdusele() {
 function incarcaProdus() {
     let id = window.location.search.split(/=/g)[1];
     let url = `https://magazinelectronic-fa84a.firebaseio.com/produse/${id}.json`;
-    fetch(url)
+    return fetch(url)
         .then(response => {
             if(response.ok) {
                 return response;
@@ -64,11 +64,84 @@ function adaugaInCos(id, nume, pret) {
         'id': id,
         'nume': nume,
         'pret': pret,
-        'cantitate': cantitate
+        'cantitate': cantitate,
+        'subtotal': pret*cantitate
     };
-    let key = 'id' + id;
-    window.localStorage.setItem(key, JSON.stringify(produs));
+    let produse = JSON.parse(localStorage.getItem('produse'));
+    if(produse) {
+        produse.push(produs);
+    } else {
+        produse = [produs];
+    }
+    //console.log(produse);
+    window.localStorage.setItem('produse',JSON.stringify(produse));
     deschideModal(produs.nume);
+}
+
+function afiseazaCos() {
+    let produse = JSON.parse(localStorage.getItem('produse'));
+    //console.log(produse);
+    if(produse) {
+        let tabel = document.querySelector('#infoCos tbody');
+        let total = 0;
+        tabel.innerHTML = '';
+        for(let i in produse) {
+            tabel.innerHTML += `
+                <tr>
+                    <td><a href="details.html?id=${produse[i].id}">${produse[i].nume}</a></td>
+                    <td>${produse[i].pret} &euro;</td>
+                    <td>
+                        <button onclick="scadeCantitate(${i});">-</button> 
+                        ${produse[i].cantitate} 
+                        <button onclick="cresteCantitate(${i});">+</button> 
+                    </td>
+                    <td>${produse[i].subtotal} &euro;</td>
+                    <td><button onclick="stergeProdusCos(${i});">Șterge</button></td>
+                </tr>
+            `;
+            total += produse[i].subtotal;
+        }
+        let info = document.querySelector('#totalAjax');
+        info.innerHTML = `
+            <p>Număr produse: ${produse.length}</p>
+            <p>TVA: 0%</p>
+            <p>Transport: 0 &euro;</p>
+            <p>TOTAL: ${total} &euro;</p>
+        `;
+    } else {
+        console.log('cos gol');
+    }
+}
+
+function scadeCantitate(i) {
+    let produse = JSON.parse(localStorage.getItem('produse'));
+    if(produse) {
+        if(produse[i].cantitate > 0) {
+            produse[i].cantitate--;
+            produse[i].subtotal = produse[i].cantitate * produse[i].pret;
+            window.localStorage.setItem('produse',JSON.stringify(produse));
+            afiseazaCos();
+        }
+    }
+}
+
+function cresteCantitate(i) {
+    let produse = JSON.parse(localStorage.getItem('produse'));
+    if(produse) {
+        produse[i].cantitate++;
+        produse[i].subtotal = produse[i].cantitate * produse[i].pret;
+        window.localStorage.setItem('produse',JSON.stringify(produse));
+        afiseazaCos();
+    }
+}
+
+function stergeProdusCos(i) {
+    let produse = JSON.parse(localStorage.getItem('produse'));
+    if(produse) {
+        produse.splice(i,1);
+        window.localStorage.setItem('produse',JSON.stringify(produse));
+        afiseazaCos();
+    }    
 }
 
 function deschideModal(nume) {
